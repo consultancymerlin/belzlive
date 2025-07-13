7165faeed55798ea783c7b98a35a43e9ceb74d97
consultancymerlin <consultancymerlin@gmail.com>
2025-07-13 20:44:59 +0100

    Create passwords.js

---
+++ passwords.js
@@ -0,0 +1,12 @@
+const validPasswords = [
+  "zK38xVu7", "tQ19pMd4", "Lk90AzV3", "Rt85sWn2", "bP76yKj9",
+  "Ew44uNc8", "Xa21tRg5", "Nq39jLv2", "Cm65xFr1", "Yd73qHo6",
+  "Ua84sWl9", "Vm28nGe3", "Tk45rPx7", "Hr93vMd1", "Gb67yKs2",
+  "Lt32qFb8", "Wn76aZk3", "Kx19jRt6", "Oa58xLu7", "Bp40vYc1",
+  "Ql61nFe2", "Df87pMz4", "Ra92xTo8", "Yb74kHq5", "Vs56rLc9",
+  "Mx13aPw2", "Jn48qGy1", "Kg62vNz7", "Ls95yCo3", "Wp30zAk8",
+  "Nb20rTl4", "Xe78pFq1", "Tr84xHd6", "Oj99kMw3", "Hl26aPv5",
+  "Va33yRn9", "Ru59qXj2", "Bf42pMy6", "Sk88tOl1", "Yn61aWr7",
+  "Po36xVf4", "Lj17qKg9", "Mw85nTs3", "Gt23zWp8", "Hz70aNc6",
+  "Ux64yFd1", "Zr95pLo2", "Yc31vTw7", "Kb50nQm4", "Ns39xHy5"
+];
